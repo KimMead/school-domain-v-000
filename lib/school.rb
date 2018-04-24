a163c9bdebd76d4ee313fe169ab2cb79a0bf86c5
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-24 00:08:49 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,11 +8,7 @@ def initialize(name)
 end
 
 def add_student(name, level)
-  if @roster[level] != nil
-    @roster[level] << name
-  else
-    @roster[level] << [name]
-  end
+roster[level] << name 
 end
 end
 
